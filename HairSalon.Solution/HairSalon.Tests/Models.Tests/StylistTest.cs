120bd2ce2113b5bb2aee3ab461cb1bbffde8653d
LydianLights <rane.fields@gmail.com>
2017-10-27 09:48:49 -0700

    Add equality method

--- HairSalon.Solution/HairSalon.Tests/Models.Tests/StylistTest.cs
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/StylistTest.cs
@@ -17,5 +17,25 @@ namespace HairSalon.Models.Tests
 
         Assert.AreEqual(0, result);
     }
+    [TestMethod]
+    public void Equals_BothHaveSameProperties_True()
+    {
+        Stylist stylist1 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
+        Stylist stylist2 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
+
+        bool result = stylist1.Equals(stylist2);
+
+        Assert.AreEqual(true, result);
+    }
+    [TestMethod]
+    public void Equals_BothDontHaveSameProperties_False()
+    {
+        Stylist stylist1 = new Stylist("Harry Styles", "555-4247", "ultrastyles42@yahoo.com");
+        Stylist stylist2 = new Stylist("Katie Cutter", "555-2467", "xXxkatethegreatxXx@aol.com");
+
+        bool result = stylist1.Equals(stylist2);
+
+        Assert.AreEqual(false, result);
+    }
   }
 }
--- HairSalon.Solution/HairSalon/Models/Stylist.cs
+++ HairSalon.Solution/HairSalon/Models/Stylist.cs
@@ -5,6 +5,7 @@ namespace HairSalon.Models
 {
   public class Stylist
   {
+      public int? DatabaseId {get; private set;}
       public string Name {get; private set;}
       public string Phone {get; private set;}
       public string Email {get; private set;}
@@ -14,6 +15,32 @@ namespace HairSalon.Models
           Name = name;
           Phone = phone;
           Email = email;
+          DatabaseId = null;
+      }
+
+      public override bool Equals(Object other)
+      {
+          if (!(other is Stylist))
+          {
+              return false;
+          }
+          else
+          {
+              Stylist otherStylist = (Stylist)other;
+              return (
+                this.Name == otherStylist.Name &&
+                this.Phone == otherStylist.Phone &&
+                this.Email == otherStylist.Email
+              );
+          }
+      }
+
+      public override int GetHashCode()
+      {
+          return
+            this.Name.GetHashCode() +
+            this.Phone.GetHashCode() +
+            this.Email.GetHashCode();
       }
 
       public static List<Stylist> GetAll()

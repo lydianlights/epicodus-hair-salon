4227712bab6a385f8c8709ead0c8f691701be866
LydianLights <rane.fields@gmail.com>
2017-10-27 12:37:09 -0700

    Add client class with methods to interact with database

--- HairSalon.Solution/HairSalon.Tests/Models.Tests/ClientTest.cs
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/ClientTest.cs
@@ -6,12 +6,89 @@ using HairSalon.Models;
 namespace HairSalon.Models.Tests
 {
     [TestClass]
-    public class ClientTest
+    public class ClientTest : IDisposable
     {
-        // [TestMethod]
-        // public void Method_Description_ExpectedValue()
-        // {
-        //
-        // }
+        public ClientTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost; user id=root; password=root; port=8889; database=rane_fields_test;";
+        }
+        public void Dispose()
+        {
+            Client.ClearAll();
+        }
+
+        [TestMethod]
+        public void GetAll_DatabaseIsEmptyAtFirst_0()
+        {
+            List<Client> allClients = Client.GetAll();
+
+            int result = allClients.Count;
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Equals_BothHaveSameProperties_True()
+        {
+            Client client1 = new Client("Dude McBuff", "555-2833", 0);
+            Client client2 = new Client("Dude McBuff", "555-2833", 0);
+
+            Assert.AreEqual(client1, client2);
+        }
+        [TestMethod]
+        public void Equals_BothDontHaveSameProperties_False()
+        {
+            Client client1 = new Client("Dude McBuff", "555-2833", 0);
+            Client client2 = new Client("Meghan McMannicure", "555-6245", 0);
+
+            Assert.AreNotEqual(client1, client2);
+        }
+        [TestMethod]
+        public void Equals_ComparingDifferentObjects_False()
+        {
+            Client client1 = new Client("A Cat", "555-6369", 0);
+            string client2 = "Meow";
+
+            Assert.AreNotEqual(client1, client2);
+        }
+        [TestMethod]
+        public void Save_SavesObjectToDatabase_ObjectIsSaved()
+        {
+            Client localClient = new Client("Meghan McMannicure", "555-6245", 0);
+            localClient.Save();
+            Client databaseClient = Client.GetAll()[0];
+
+            Assert.AreEqual(localClient, databaseClient);
+        }
+        [TestMethod]
+        public void Save_SavesMultipleObjectsToDatabase_ObjectsAreSaved()
+        {
+            Client localClient1 = new Client("Dude McBuff", "555-2833", 0);
+            localClient1.Save();
+            Client localClient2 = new Client("Meghan McMannicure", "555-6245", 0);
+            localClient2.Save();
+            List<Client> allLocalClients = new List<Client> {localClient1, localClient2};
+            List<Client> allDatabaseClients = Client.GetAll();
+
+            CollectionAssert.AreEqual(allLocalClients, allDatabaseClients);
+        }
+        [TestMethod]
+        public void FindById_GetsSpecificObjectFromDatabase_Object()
+        {
+            Client localClient1 = new Client("Dude McBuff", "555-2833", 0);
+            localClient1.Save();
+            Client localClient2 = new Client("Meghan McMannicure", "555-6245", 0);
+            localClient2.Save();
+            Client localClient3 = new Client("A Cat", "555-6369", 0);
+            localClient3.Save();
+            Client databaseClient2 = Client.FindById((int)localClient2.Id);
+
+            Assert.AreEqual(localClient2, databaseClient2);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void FindById_ObjectDoesntExistInDatabase_Exception()
+        {
+            Client databaseClient2 = Client.FindById(0);
+        }
     }
 }
--- HairSalon.Solution/HairSalon/Models/Client.cs
+++ HairSalon.Solution/HairSalon/Models/Client.cs
@@ -1,10 +1,148 @@
 using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
     public class Client
     {
+        public int? Id {get; private set;}
+        public string Name {get; private set;}
+        public string Phone {get; private set;}
+        public int StylistId {get; private set;}
 
+        public const string SqlTable = "clients";
+
+        public Client(string name, string phone, int stylistId)
+        {
+            Name = name;
+            Phone = phone;
+            StylistId = stylistId;
+            Id = null;
+        }
+        public Client(int id, string name, string phone, int stylistId) : this(name, phone, stylistId)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(Object other)
+        {
+            if (!(other is Client))
+            {
+                return false;
+            }
+            else
+            {
+                Client otherClient = (Client)other;
+                return (
+                    this.Name == otherClient.Name &&
+                    this.Phone == otherClient.Phone &&
+                    this.StylistId == otherClient.StylistId
+                );
+            }
+        }
+        public override int GetHashCode()
+        {
+            return
+                this.Name.GetHashCode() +
+                this.Phone.GetHashCode() +
+                this.StylistId.GetHashCode();
+        }
+
+        public static List<Client> GetAll()
+        {
+            List<Client> output = new List<Client> {};
+            MySqlConnection conn = DB.Connection;
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = $@"SELECT * FROM {SqlTable};";
+            MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+            while (rdr.Read())
+            {
+                int id = rdr.GetInt32(0);
+                string name = rdr.GetString(1);
+                string phone = rdr.GetString(2);
+                int stylistId = rdr.GetInt32(3);
+                Client client = new Client(id, name, phone, stylistId);
+                output.Add(client);
+            }
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return output;
+        }
+
+        public static void ClearAll()
+        {
+            MySqlConnection conn = DB.Connection;
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = $@"DELETE FROM {SqlTable};";
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+
+        public static Client FindById(int id)
+        {
+            MySqlConnection conn = DB.Connection;
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = $@"SELECT * FROM {SqlTable} WHERE id = @Id;";
+            cmd.Parameters.Add(new MySqlParameter("@Id", id));
+
+            string name = null;
+            string phone = null;
+            int stylistId = 0;
+            var rdr = cmd.ExecuteReader() as MySqlDataReader;
+            if(rdr.Read())
+            {
+                name = rdr.GetString(1);
+                phone = rdr.GetString(2);
+                stylistId = rdr.GetInt32(3);
+            }
+            else
+            {
+                throw new InvalidOperationException($"No entry exists in table '{SqlTable}' with id '{id}'");
+            }
+            Client output = new Client(id, name, phone, stylistId);
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+            return output;
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection;
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = $@"INSERT INTO {SqlTable} (name, phone, stylist_id) VALUES (@Name, @Phone, @StylistId)";
+            cmd.Parameters.Add(new MySqlParameter("@Name", Name));
+            cmd.Parameters.Add(new MySqlParameter("@Phone", Phone));
+            cmd.Parameters.Add(new MySqlParameter("@StylistId", StylistId));
+            cmd.ExecuteNonQuery();
+            this.Id = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
     }
 }

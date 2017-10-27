44709123c89a1e9d24cff6bf55f6d0263fa66ab9
LydianLights <rane.fields@gmail.com>
2017-10-27 08:17:09 -0700

    Add project template files

---
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/_MyClassTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HairSalon.Models;
+
+namespace HairSalon.Models.Tests
+{
+  [TestClass]
+  public class _MyClassTest
+  {
+    [TestMethod]
+    public void Method_Description_ExpectedValue()
+    {
+
+    }
+  }
+}

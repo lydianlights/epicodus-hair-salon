4b2b70b767d10e624d3bf04422f8306936aae807
LydianLights <rane.fields@gmail.com>
2017-10-27 10:48:59 -0700

    Add save method

--- HairSalon.Solution/HairSalon.Tests/Models.Tests/StylistTest.cs
+++ HairSalon.Solution/HairSalon.Tests/Models.Tests/StylistTest.cs
@@ -5,37 +5,55 @@ using HairSalon.Models;
 
 namespace HairSalon.Models.Tests
 {
-  [TestClass]
-  public class StylistTest
-  {
-    [TestMethod]
-    public void GetAll_DatabaseIsEmptyAtFirst_0()
+    [TestClass]
+    public class StylistTest
     {
-        List<Stylist> allStylists = Stylist.GetAll();
+        public StylistTest()
+        {
+            DBConfiguration.ConnectionString = "server=localhost; user id=root; password=root; port=8889; database=rane_fields_test;";
+        }
 
-        int result = allStylists.Count;
+        [TestMethod]
+        public void GetAll_DatabaseIsEmptyAtFirst_0()
+        {
+            List<Stylist> allStylists = Stylist.GetAll();
 
-        Assert.AreEqual(0, result);
+            int result = allStylists.Count;
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void Equals_BothHaveSameProperties_True()
+        {
+            Stylist stylist1 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
+            Stylist stylist2 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
+
+            Assert.AreEqual(stylist1, stylist2);
+        }
+        [TestMethod]
+        public void Equals_BothDontHaveSameProperties_False()
+        {
+            Stylist stylist1 = new Stylist("Harry Styles", "555-4247", "ultrastyles42@yahoo.com");
+            Stylist stylist2 = new Stylist("Katie Cutter", "555-2467", "xXxkatethegreatxXx@aol.com");
+
+            Assert.AreNotEqual(stylist1, stylist2);
+        }
+        [TestMethod]
+        public void Equals_ComparingDifferentObjects_False()
+        {
+            Stylist stylist1 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
+            int stylist2 = 66;
+
+            Assert.AreNotEqual(stylist1, stylist2);
+        }
+        [TestMethod]
+        public void Save_SavesObjectToDatabase_ObjectIsSaved()
+        {
+            Stylist localStylist = new Stylist("Harry Styles", "555-4247", "ultrastyles42@yahoo.com");
+            localStylist.Save();
+            Stylist databaseStylist = Stylist.GetAll()[0];
+
+            Assert.AreEqual(localStylist, databaseStylist);
+        }
     }
-    [TestMethod]
-    public void Equals_BothHaveSameProperties_True()
-    {
-        Stylist stylist1 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
-        Stylist stylist2 = new Stylist("Barry the Chopper", "555-3622", "experiment66@amestrismail.com");
-
-        bool result = stylist1.Equals(stylist2);
-
-        Assert.AreEqual(true, result);
-    }
-    [TestMethod]
-    public void Equals_BothDontHaveSameProperties_False()
-    {
-        Stylist stylist1 = new Stylist("Harry Styles", "555-4247", "ultrastyles42@yahoo.com");
-        Stylist stylist2 = new Stylist("Katie Cutter", "555-2467", "xXxkatethegreatxXx@aol.com");
-
-        bool result = stylist1.Equals(stylist2);
-
-        Assert.AreEqual(false, result);
-    }
-  }
 }
--- HairSalon.Solution/HairSalon/Models/Stylist.cs
+++ HairSalon.Solution/HairSalon/Models/Stylist.cs
@@ -1,52 +1,75 @@
 using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
-  public class Stylist
-  {
-      public int? DatabaseId {get; private set;}
-      public string Name {get; private set;}
-      public string Phone {get; private set;}
-      public string Email {get; private set;}
+    public class Stylist
+    {
+        public int? Id {get; private set;}
+        public string Name {get; private set;}
+        public string Phone {get; private set;}
+        public string Email {get; private set;}
 
-      public Stylist(string name, string phone, string email)
-      {
-          Name = name;
-          Phone = phone;
-          Email = email;
-          DatabaseId = null;
-      }
+        public const string SqlTable = "stylists";
 
-      public override bool Equals(Object other)
-      {
-          if (!(other is Stylist))
-          {
-              return false;
-          }
-          else
-          {
-              Stylist otherStylist = (Stylist)other;
-              return (
-                this.Name == otherStylist.Name &&
-                this.Phone == otherStylist.Phone &&
-                this.Email == otherStylist.Email
-              );
-          }
-      }
+        public Stylist(string name, string phone, string email)
+        {
+            Name = name;
+            Phone = phone;
+            Email = email;
+            Id = null;
+        }
 
-      public override int GetHashCode()
-      {
-          return
-            this.Name.GetHashCode() +
-            this.Phone.GetHashCode() +
-            this.Email.GetHashCode();
-      }
+        public override bool Equals(Object other)
+        {
+            if (!(other is Stylist))
+            {
+                return false;
+            }
+            else
+            {
+                Stylist otherStylist = (Stylist)other;
+                return (
+                    this.Name == otherStylist.Name &&
+                    this.Phone == otherStylist.Phone &&
+                    this.Email == otherStylist.Email
+                );
+            }
+        }
 
-      public static List<Stylist> GetAll()
-      {
-          List<Stylist> output = new List<Stylist> {};
-          return output;
-      }
-  }
+        public override int GetHashCode()
+        {
+            return
+                this.Name.GetHashCode() +
+                this.Phone.GetHashCode() +
+                this.Email.GetHashCode();
+        }
+
+        public static List<Stylist> GetAll()
+        {
+            List<Stylist> output = new List<Stylist> {};
+            return output;
+        }
+
+        public void Save()
+        {
+            MySqlConnection conn = DB.Connection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand() as MySqlCommand;
+            cmd.CommandText = $@"INSERT INTO {SqlTable} (name, phone, email) VALUES (@Name, @Phone, @Email)";
+            cmd.Parameters.Add(new MySqlParameter("@Name", Name));
+            cmd.Parameters.Add(new MySqlParameter("@Phone", Phone));
+            cmd.Parameters.Add(new MySqlParameter("@Email", Email));
+            cmd.ExecuteNonQuery();
+            this.Id = (int)cmd.LastInsertedId;
+
+            conn.Close();
+            if (conn != null)
+            {
+                conn.Dispose();
+            }
+        }
+    }
 }
